6231e5606ddea4fdee35ffa56a796b9e0f1306e7
Matthew <TekNoir@users.noreply.github.com>
2017-09-18 14:44:22 -0500

    Scaffolding for project.

--- designs.js
+++ designs.js
@@ -2,7 +2,10 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+let height = 1;
+let width = 1;
+let color = "#000000";
+const table = $("#pixel_canvas");
 
-function makeGrid() {
-  // Your code goes here!
-}
+function makeGrid(height, width) {}
+function clearGrid() {}
