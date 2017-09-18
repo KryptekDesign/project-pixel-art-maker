db42f5c6a83fa6b8f9db5839661f54ffc824f509
Matthew <TekNoir@users.noreply.github.com>
2017-09-18 18:51:36 -0500

    Implement makeGrid

--- designs.js
+++ designs.js
@@ -4,10 +4,22 @@
 // When size is submitted by the user, call makeGrid()
 let height = 1;
 let width = 1;
-let color = "#000000";
-const table = $("#pixel_canvas");
+let color = '#000000';
+const table = $('#pixel_canvas');
 
-function makeGrid(height, width) {}
+function makeGrid(height, width) {
+  clearGrid();
+	for (let row = 0; row < height; row++) {
+    let tableRow = '<tr>';
+		for (let col = 0; col < width; col++) {
+			tableRow = tableRow + '<td></td>';
+    }
+    tableRow = tableRow + '</tr>';
+    table.append(tableRow);
+	}
+}
+
+// Clears Table-based grid.
 function clearGrid() {
   table.empty();
 }
