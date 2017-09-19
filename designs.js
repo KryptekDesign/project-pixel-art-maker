3e05371a40204acaa9274f3b667180ece3e95517
Matthew <TekNoir@users.noreply.github.com>
2017-09-18 19:01:48 -0500

    Implement submit function.

--- designs.js
+++ designs.js
@@ -4,22 +4,32 @@
 // When size is submitted by the user, call makeGrid()
 let height = 1;
 let width = 1;
-let color = '#000000';
-const table = $('#pixel_canvas');
+let color = "#000000";
+const table = $("#pixel_canvas");
 
+// Create Table-based grid using user supplied height x width.
 function makeGrid(height, width) {
   clearGrid();
-	for (let row = 0; row < height; row++) {
-    let tableRow = '<tr>';
-		for (let col = 0; col < width; col++) {
-			tableRow = tableRow + '<td></td>';
+  for (let row = 0; row < height; row++) {
+    let tableRow = "<tr>";
+    for (let col = 0; col < width; col++) {
+      tableRow = tableRow + "<td></td>";
     }
-    tableRow = tableRow + '</tr>';
+    tableRow = tableRow + "</tr>";
     table.append(tableRow);
-	}
+  }
 }
 
 // Clears Table-based grid.
 function clearGrid() {
   table.empty();
 }
+
+//
+$("#sizePicker").submit(function(e) {
+  e.preventDefault();
+  let height = $("#input_height").val();
+  let width = $("#input_width").val();
+
+  makeGrid(height, width);
+});
